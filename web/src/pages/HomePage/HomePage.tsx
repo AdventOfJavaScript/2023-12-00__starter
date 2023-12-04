357deec0cc74e9ec55ca33e2b992c14a4d034965
Cameron <69396582+camer0nluo@users.noreply.github.com>
2023-12-03 20:15:58 -0800

    feature: add homepage layout

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,31 +1,35 @@
 import { Auth0Provider } from '@auth0/auth0-react'
 
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+import AuthLayout from 'src/layouts/AuthLayout/AuthLayout'
 
 import LoginButton from './Login'
 import LogoutButton from './Logout'
+// import { Link, routes } from '@redwoodjs/router'
 
 const HomePage = () => {
   return (
     <>
-      <Auth0Provider
-        domain={process.env.AUTH0_DOMAIN}
-        clientId={process.env.AUTH0_CLIENT_ID}
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
-        <MetaTags title="Home" description="Home Page" />
+      <AuthLayout>
+        <Auth0Provider
+          domain={process.env.AUTH0_DOMAIN}
+          clientId={process.env.AUTH0_CLIENT_ID}
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+          }}
+        >
+          <MetaTags title="Home" description="Home Page" />
 
-        <h1>Login Here</h1>
-        <p>
-          {/* My default route is named <code>home</code>, link to me with `
+          <h1>Login Here</h1>
+          <p>
+            {/* My default route is named <code>home</code>, link to me with `
           <Link to={routes.home()}>Home</Link> */}
-          <LoginButton />
-          <LogoutButton />
-        </p>
-      </Auth0Provider>
+            <LoginButton />
+            <LogoutButton />
+          </p>
+        </Auth0Provider>
+      </AuthLayout>
     </>
   )
 }
